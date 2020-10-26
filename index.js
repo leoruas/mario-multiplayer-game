fff845ddf9e187fbc21675daad44895c54275f07
leoruas <leo.coelho.ruas@gmail.com>
2020-10-26 12:36:13 -0300

    multiple players can now connect and disconnect

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/** Multiplayer game example -> https://code.tutsplus.com/tutorials/create-a-multiplayer-pirate-shooter-game-in-your-browser--cms-23311
+ *  Github repo -> https://github.com/tutsplus/tutsplus-glitch-multiplayer
+ * Types of socket io calls -> https://gist.github.com/OmarShehata/a67499b29c65c8b84f74760652890bd8
+ */
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
@@ -9,15 +13,32 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
+var players = {}  
 io.on('connect', (socket) => {
-    console.log("New player connected");
+    console.log("New client has connected with id:", socket.id);
+
+    socket.on('new-player', function(data) { 
+        console.log("Player data", data);
+        players[socket.id] = data;
+
+        io.emit('update-players', players); //emit to ALL connected sockets
+        // socket.broadcast.emit('create-player', data)
+        //Calling socket.broadcast.emit sends it to every client connected to the server, except that one socket it was called on.
+    })
 
     socket.on('disconnect', () => {
         console.log("Player disconnected");
+        delete players[socket.id];
+        console.log('new players object', players);
+        io.emit('update-players', players);
     })
+
+    // socket.on('addPoint', (id) => {
+    //     socket.emit("addPoint", id);
+    // })
 })
 
-const port = 4000;
+const port = 8080;
 http.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
--- public/controller.js
+++ public/controller.js
@@ -27,12 +27,61 @@ var gameArea = {
 }
 
 var player;
+var otherPlayers = {};
 var food;
+var scoreList = document.getElementById("scoreList");
+
+socket.on('addPoint', function (id) {
+    player.score++;
+    scoreList.childNodes[id].innerText = `Player ${id + 1}: ${player.score} pontos`
+});
+
+socket.on('update-players', function (players) {
+    var playersFound = {};
+    for(var id in players) {
+        if(!Object.keys(otherPlayers).includes(id) && socket.id !== id) {
+            var data = players[id];
+            console.log("add new player", data);
+            var newPlayer = new Player(data.x, data.y, data.speed, "assets/player1.png");
+            otherPlayers[id] = newPlayer;
+        }
+        playersFound[id] = true;
+    };
+
+    for(var id in otherPlayers) {
+        if(!Object.keys(playersFound).includes(id)){
+            console.log("player disconnected ", id);
+            delete otherPlayers[id];
+        }
+    }
+});
 
 function startGame() {
     this.gameArea.start();
-    player = new Player(0, 0, 7, "assets/player1.png");
-    food = new Food(100, 100);
+    player = new Player(100, 100, 7, "assets/player1.png");
+    socket.emit('new-player', {
+        x: player.x,
+        y: player.y,
+        speed: player.speed
+    })
+
+    x = Math.floor(Math.random() * gameArea.canvas.width);
+    if (x >= 16) x -= 16;
+    y = Math.floor(Math.random() * gameArea.canvas.height);
+    if (y >= 16) y -= 16
+    food = new Food(x, y);
+
+    // var scoreList = document.getElementById("scoreList");
+    // //foreach player
+    // const newScore = (id) => {
+    //     const item = document.createElement("li");
+    //     item.id = `player${id}`
+    //     item.innerText = `Player ${id + 1}: 0 pontos`
+        
+    //     return item;
+    // };
+
+    // scoreList.append(newScore(0));
 };
 
 function Player(x, y, speed, src) {
@@ -44,17 +93,31 @@ function Player(x, y, speed, src) {
     this.src = src;
     this.image = new Image();
     this.image.src = this.src;
+    this.width = this.image.width;
+    this.height = this.image.height;
+    this.isFlipped = false;
+    this.score = 0;
 
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
     }
-    
+
     this.update = function () {
         ctx = gameArea.ctx;
-        
-        ctx.drawImage(this.image, this.x, this.y);
-    }    
+        if (this.isFlipped) {
+            ctx.save();
+            ctx.scale(-1, 1);
+        }
+
+        ctx.drawImage(this.image, this.isFlipped ? -this.x : this.x, this.y);
+
+        if (this.isFlipped) ctx.restore();
+    }
+
+    this.addPoint = function (id) {
+        socket.emit('addPoint', id)
+    }
 }
 
 function Food(x, y) {
@@ -62,10 +125,45 @@ function Food(x, y) {
     this.y = y;
     this.image = new Image();
     this.image.src = "assets/food.png"
+    this.width = this.image.width;
+    this.height = this.image.height;
 
-    this.update = function() {
+    this.respawn = function () {
+        this.x = Math.floor(Math.random() * (gameArea.canvas.width - this.width));
+        this.y = Math.floor(Math.random() * (gameArea.canvas.height - this.height));
+
         ctx = gameArea.ctx;
 
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+
+    this.wasEaten = function (pl) {
+        var myLeft = this.x;
+        var myRight = this.x + this.width;
+
+        var myTop = this.y;
+        var myBottom = this.y + this.height;
+
+        var pLeft = pl.x;
+        var pRight = pl.x + pl.width;
+
+        var pTop = pl.y;
+        var pBottom = pl.y + pl.height;
+
+        if (myBottom >= pTop &&
+            myTop <= pBottom &&
+            myRight >= pLeft &&
+            myLeft <= pRight
+        ) {
+            pl.addPoint(0);
+            return true;
+        }
+
+        return false;
+    }
+
+    this.update = function () {
+        ctx = gameArea.ctx;
         ctx.drawImage(this.image, this.x, this.y);
     }
 }
@@ -73,22 +171,32 @@ function Food(x, y) {
 function updateGameArea() {
     gameArea.clear();
 
+    if (food.wasEaten(player)) food.respawn();
+
     checkKeys();
     player.newPos();
     player.update();
     food.update();
+    for(var id in otherPlayers)
+        otherPlayers[id].update();
 }
 
-function checkKeys (){
+function checkKeys() {
     player.speedX = 0;
     player.speedY = 0;
 
-    if(gameArea.keys["ArrowUp"]) player.speedY = -player.speed;
-    if(gameArea.keys["ArrowDown"]) player.speedY = player.speed;
-    if(gameArea.keys["ArrowRight"]) player.speedX = player.speed;
-    if(gameArea.keys["ArrowLeft"]) player.speedX = -player.speed
+    if (gameArea.keys["ArrowUp"]) player.speedY = -player.speed;
+    if (gameArea.keys["ArrowDown"]) player.speedY = player.speed;
+    if (gameArea.keys["ArrowRight"]) {
+        player.speedX = player.speed;
+        player.isFlipped = false;
+    }
+    if (gameArea.keys["ArrowLeft"]) {
+        player.speedX = -player.speed;
+        player.isFlipped = true;
+    }
 }
 
-function log() {
-    console.log(gameArea);
-}
+// function log() {
+//     console.log(gameArea);
+// }
